ad688c0b8a2f1e8228fd3111b59dbd4744d021b9
Error-LinuxKernelCrashed <shy2354538846@outlook.com>
2021-01-25 18:34:31 +0800

    Create Maven Project For Desktop Controller Software

---
+++ EmoePSUController/src/main/java/com/emoe/iot/App.java
@@ -0,0 +1,13 @@
+package com.emoe.iot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
